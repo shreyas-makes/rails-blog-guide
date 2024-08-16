987a1ec849d8d1dbb309b62474fb70dee76c3204
Shreyas <shreyas314159@gmail.com>
2024-08-16 10:17:46 +0100

    new route should be processed before show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   root "blog_posts#index"
 
+  get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+
   get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  
 
 
 
