bebca47a396e72e5631fff4184563b5c34d0202b
Shreyas <shreyas314159@gmail.com>
2024-08-16 12:35:04 +0100

    used a button_to helper for the destroy button on the edit page
    destroy action added to the BlogPost Controller as well as a redirect_to helper for the user to go back to the index route after the change is made
    
    added a turbo confirm modal whenever a user is deleting a page with a 'are you sure?'
    
    refactoring the 6+1 routes in the routes.rb file. as these are basic CRUD endpoints, used the resources function to refactor the code. redundant to write this everytime. DRY principle here
    
    added a before_action to the controllers for BlogPosts controller and defined the set_blog_post method, and in this way, i was able to reuse the code across the actions for '@blog_post = BlogPost.find(params[:id])'
    
    removed the rescue error as that's optional

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
 
   root "blog_posts#index"
 
-  get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  patch "blog_posts/:id", to: "blog_posts#update"
-  get "blog_posts/:id/edit", to: "blog_posts#edit", as: :blog_post_edit
-  post "blog_posts", to: "blog_posts#create", as: :blog_posts
+  resources :blog_posts
+
   
 
 
